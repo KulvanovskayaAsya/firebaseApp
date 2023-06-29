83dd5f4ef3b36e6f687e2ddec7b2d4a973d29584
Kulvanovskaya Anastasia <nastena.kulvanovskaya.02@mail.ru>
2023-06-29 16:13:01 +0300

    Try to correct VAPID KEY

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-messaging.js');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYmt6s0xxvoCHtsXSflHczhMZ43GP8CQ-",
+  apiKey: "AIzaSyDY-mt6s0xxvoCHtsXSflHczhMZ43GP8CQ",
   authDomain: "grandcasino-47124.firebaseapp.com",
   projectId: "grandcasino-47124",
   messagingSenderId: "1055046129020",
