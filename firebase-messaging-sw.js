5014b3c45c15d196dbf13062257bf6a189bba582
Kulvanovskaya Anastasia <nastena.kulvanovskaya.02@mail.ru>
2023-06-29 16:06:50 +0300

    кккк

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,13 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-messaging.js');
 
-firebase.initializeApp({
-    messagingSenderId: '1055046129020'
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyDYmt6s0xxvoCHtsXSflHczhMZ43GP8CQ-",
+  authDomain: "grandcasino-47124.firebaseapp.com",
+  projectId: "grandcasino-47124",
+  messagingSenderId: "1055046129020",
+  appId: "1:1055046129020:web:82d8b2224aa908550240f9",
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
