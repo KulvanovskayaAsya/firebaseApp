3be885b8e3130d3ca86202a878de8a3e6c52639d
Kulvanovskaya Anastasia <nastena.kulvanovskaya.02@mail.ru>
2023-06-30 12:44:05 +0300

    firebase-messaging-sw

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBawUPnBPzcx4Y256Jd7EqMveSOCmhv0Yk",
+    authDomain: "fir-messages-ed5a7.firebaseapp.com",
+    projectId: "fir-messages-ed5a7",
+    storageBucket: "fir-messages-ed5a7.appspot.com",
+    messagingSenderId: "282798683267",
+    appId: "1:282798683267:web:171c04bbdea4d522d417b6"
+});
+
+const messaging = firebase.messaging();
+
+// Обработчик входящих уведомлений
+messaging.onMessage((payload) => {
+  console.log('Получено уведомление', payload);
+});
